d759c70770304d3b0c58e5ad9a12cdde650afa81
Adeboye24 <cyberseclearn24@gmail.com>
2024-08-03 15:18:37 +0100

    Add files via upload

---
+++ 2nd Trial Password Checker.py
@@ -0,0 +1,34 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/joseph/Desktop/BISWAL/Password_strength_checker.py
+Enter your password: OLUWATOYIN12
+Password strength: Moderate
+>>> 
+===== RESTART: C:/Users/joseph/Desktop/BISWAL/Password_strength_checker.py =====
+Enter your password: OLUWATOYIN12.?
+Password strength: Moderate
+>>> 
+===== RESTART: C:/Users/joseph/Desktop/BISWAL/Password_strength_checker.py =====
+Enter your password: OluwaTOYIN12.?
+Password strength: Strong
+>>> 
+===== RESTART: C:/Users/joseph/Desktop/BISWAL/Password_strength_checker.py =====
+Enter your password: password
+Password strength: Weak
+>>> 
+===== RESTART: C:/Users/joseph/Desktop/BISWAL/Password_strength_checker.py =====
+Enter your password: PassWOrd
+Password strength: Weak
+>>> 
+===== RESTART: C:/Users/joseph/Desktop/BISWAL/Password_strength_checker.py =====
+Enter your password: PassWord12.?
+Password strength: Strong
+>>> 
+===== RESTART: C:/Users/joseph/Desktop/BISWAL/Password_strength_checker.py =====
+Enter your password: Pass12
+Password strength: Weak
+>>> 
+===== RESTART: C:/Users/joseph/Desktop/BISWAL/Password_strength_checker.py =====
+Enter your password: 1DidpasswordCheckerSuccessfully12.?
+Password strength: Strong
